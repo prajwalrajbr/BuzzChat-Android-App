87d65af1a0c6bddb44a2873c60a65b9d2722fcc5
prajwalrajbr <42075029+prajwalrajbr@users.noreply.github.com>
2020-08-11 19:22:08 +0530

    Profile Activity Completed...

--- app/src/main/java/com/example/buzzchat/ProfileActivity.java
+++ app/src/main/java/com/example/buzzchat/ProfileActivity.java
@@ -4,20 +4,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.Continuation;
@@ -33,9 +38,16 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Objects;
+
+import id.zelory.compressor.Compressor;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -49,6 +61,9 @@ public class ProfileActivity extends AppCompatActivity {
     private String downloadUrl;
     private DatabaseReference userRef;
     private ProgressDialog progressDialog;
+    private boolean isImageFitToScreen;
+    private ConstraintLayout totalImage;
+    private byte[] finalImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +83,9 @@ public class ProfileActivity extends AppCompatActivity {
         profileImg = (ImageView) findViewById(R.id.profile_pic);
         saveBtn = (Button) findViewById(R.id.profileSaveBtn);
         progressDialog = new ProgressDialog(this);
+        totalImage = findViewById(R.id.imageView);
+
+        retrieveUserProfile();
 
         imgSelectBtn.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -86,7 +104,14 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        retrieveUserProfile();
+        profileImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isImageFitToScreen) {
+                    showImageFullScreen(isImageFitToScreen);
+                }
+            }
+        });
 
     }
 
@@ -95,10 +120,42 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == galleryPick && resultCode == RESULT_OK && data!=null){
+        if(requestCode == galleryPick && resultCode == RESULT_OK && data!=null) {
             imageUri = data.getData();
-            profileImg.setImageURI(imageUri);
+            CropImage.activity(imageUri)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1, 1)
+                    .start(this);
         }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if(resultCode == RESULT_OK){
+                imageUri = result.getUri();
+
+
+                File actualImageFile = new File(imageUri.getPath());
+
+
+                try {
+                    Bitmap compressedImageFile = new Compressor(this)
+                            .setMaxWidth(250)
+                            .setMaxHeight(250)
+                            .setQuality(30)
+                            .compressToBitmap(actualImageFile);
+
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    compressedImageFile.compress(Bitmap.CompressFormat.JPEG, 30, baos);
+                    finalImage = baos.toByteArray();
+
+
+                    profileImg.setImageURI(imageUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
     }
 
 
@@ -132,7 +189,7 @@ public class ProfileActivity extends AppCompatActivity {
             progressDialog.show();
 
             final StorageReference filePath = userProfileImgRef.child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid());
-            final UploadTask uploadTask = filePath.putFile(imageUri);
+            final UploadTask uploadTask = filePath.putBytes(finalImage);
 
             uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
                 @Override
@@ -248,9 +305,54 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivity(logOutIntent);
                 finishAffinity();
                 return true;
+            case android.R.id.home:
+                if(isImageFitToScreen){
+                    showImageFullScreen(isImageFitToScreen);
+                    return true;
+                }else{
+                    return super.onOptionsItemSelected(item);
+                }
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showImageFullScreen(boolean isImageIsFitToScreen){
+        if(isImageIsFitToScreen){
+            isImageFitToScreen = false;
+            RelativeLayout.LayoutParams paramss = new RelativeLayout.LayoutParams
+                    ((int) ViewGroup.LayoutParams.WRAP_CONTENT, (int) ViewGroup.LayoutParams.WRAP_CONTENT);
+
+            paramss.addRule(RelativeLayout.CENTER_HORIZONTAL);
+            totalImage.setLayoutParams(paramss);
+
+            profileImg.requestLayout();
+            profileImg.getLayoutParams().height = 650;
+            profileImg.getLayoutParams().width = 650;
+            profileImg.setScaleType(ImageView.ScaleType.FIT_XY);
+            imgSelectBtn.setVisibility(View.VISIBLE);
+            ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) totalImage.getLayoutParams();
+            params.topMargin = 210;
+
+
+        }else{
+            isImageFitToScreen = true;
+            totalImage.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+            imgSelectBtn.setVisibility(View.INVISIBLE);
+            profileImg.requestLayout();
+
+            profileImg.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
+            profileImg.getLayoutParams().height = 1050;
+
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        if(isImageFitToScreen){
+            showImageFullScreen(isImageFitToScreen);
+        }else{
+            finish();
+        }
     }
 
 }
